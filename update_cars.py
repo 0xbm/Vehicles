34196e5c25d078c3a42cf05cf32b7a77de80d311
0xbm <0xmb@protonmail.com>
2023-02-27 20:22:41 +0100

    add option to edit in db

--- update_cars.py
+++ update_cars.py
@@ -12,7 +12,7 @@ def create_connection(db_file):
     return conn
 
 
-def update_cars(conn, project):
+def update_car(conn, project):
     sql = """ UPDATE cars
               SET make = ? ,
                   model = ? ,
@@ -25,14 +25,68 @@ def update_cars(conn, project):
     conn.commit()
 
 
+def update_make(conn, project):
+    sql = """ UPDATE cars
+              SET make = ?
+
+              WHERE id = ?"""
+
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+
+def update_model(conn, project):
+    sql = """ UPDATE cars
+              SET model = ?
+
+              WHERE id = ?"""
+
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+
+def update_reg_number(conn, project):
+    sql = """ UPDATE cars
+              SET registration_number = ?
+
+              WHERE id = ?"""
+
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+
+def update_tech_insp(conn, project):
+    sql = """ UPDATE cars
+              SET technical_inspection = ?
+
+              WHERE id = ?"""
+
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+
 def number_of_vehicles():
     con = sqlite3.connect("vehicles.db")
     cursor = con.cursor()
     result = cursor.execute(
-        "select id, make, model from cars"
+        "SELECT id, make, model FROM cars"
     ).fetchall()  # returns array of tupples
     num_of_vehicles = result
     print("Number of vehicles: ", "\x1b[5;30;42m" + str(num_of_vehicles) + "\x1b[0m")
+
+
+def select_vehicles():
+    con = sqlite3.connect("vehicles.db")
+    cursor = con.cursor()
+    result = cursor.execute(
+        "SELECT id, make, model FROM cars"
+    ).fetchall()  # returns array of tupples
+    num_of_vehicles = result
+    print("Select vehicles ID: ", "\x1b[5;30;42m" + str(num_of_vehicles) + "\x1b[0m")
 
 
 def main():
@@ -40,16 +94,86 @@ def main():
     conn = create_connection(database)
 
     with conn:
-        choice = input("What would you like to do?\n1.Update car or q for quit: \n")
+        choice = input(
+            "What would you like to do?\n1.Update make \n2.Update model \n3.Update reg number \n4.Update tech_insp \n5. Update whole car or q for quit: \n"
+        )
         match choice:
             case "1":
+                select_vehicles()
+
+                make = input("Car make: ")
+                # model = input("Car model: ")
+                # reg_number = input("Registration number: ")
+                # tech_insp = input("Technical inspection date (ones in a year): ")
+                # number_of_vehicles()
+                #
+                id = input("Select id to update: ")
+                update_make(
+                    conn,
+                    (
+                        make.capitalize(),
+                        # model.capitalize(),
+                        # reg_number.upper(),
+                        # tech_insp,
+                        id,
+                    ),
+                )
+                main()
+            case "2":
+                select_vehicles()
+                model = input("Car model: ")
+                id = input("Select id to update: ")
+                update_model(
+                    conn,
+                    (
+                        # make.capitalize(),
+                        model.capitalize(),
+                        # reg_number.upper(),
+                        # tech_insp,
+                        id,
+                    ),
+                )
+                main()
+            case "3":
+                select_vehicles()
+                reg_number = input("Registration number: ")
+                id = input("Select id to update: ")
+                update_reg_number(
+                    conn,
+                    (
+                        # make.capitalize(),
+                        # model.capitalize(),
+                        reg_number.upper(),
+                        # tech_insp,
+                        id,
+                    ),
+                )
+                main()
+            case "4":
+                select_vehicles()
+                tech_insp = input("Technical inspection date (ones in a year): ")
+                id = input("Select id to update: ")
+                update_tech_insp(
+                    conn,
+                    (
+                        # make.capitalize(),
+                        # model.capitalize(),
+                        # reg_number.upper(),
+                        tech_insp,
+                        id,
+                    ),
+                )
+                main()
+            case "5":
+                select_vehicles()
+
                 make = input("Car make: ")
                 model = input("Car model: ")
                 reg_number = input("Registration number: ")
                 tech_insp = input("Technical inspection date (ones in a year): ")
                 number_of_vehicles()
                 id = input("Select id to update: ")
-                update_cars(
+                update_car(
                     conn,
                     (
                         make.capitalize(),
