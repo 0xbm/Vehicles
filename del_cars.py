584b063109a6708e4370b17ac90b8f3aaf1469c6
0xbm <0xmb@protonmail.com>
2023-03-01 12:09:01 +0100

    add func to delete cars

--- del_cars.py
+++ del_cars.py
@@ -1,1 +1,41 @@
-#!/usr/bin/env python3
+import sqlite3
+from sqlite3 import Error
+from update_cars import select_vehicles
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def delete(conn, id):
+    sql = "DELETE FROM cars WHERE id=?"
+    cur = conn.cursor()
+    cur.execute(sql, (id,))
+    conn.commit()
+
+
+def delete_all(conn):
+    sql = "DELETE FROM asd"
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+
+def main():
+    database = r"vehicles.db"
+    conn = create_connection(database)
+    with conn:
+        select_vehicles()
+        id = input("Select id to update: ")
+        delete(conn, id)
+        # delete_all(conn);
+
+
+if __name__ == "__main__":
+    main()
