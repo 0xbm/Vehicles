beac7ef4260ae11c1256d475e1a6d5de4115fd1b
0xbm <0xmb@protonmail.com>
2023-04-14 19:19:21 +0200

    add table to card

--- cards/template_card.py
+++ cards/template_card.py
@@ -4,7 +4,77 @@ from openpyxl import Workbook
 import calendar
 import openpyxl
 import datetime
+from openpyxl.worksheet.table import Table
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.utils import get_column_letter
 
+class Style:
+    def create_months(self, wb=Workbook()):
+        # wb = Workbook()
+        # ws = wb.active
+        months = [calendar.month_name[i] for i in range(1, 13)]
+
+        for worksheet in months:
+            wb.create_sheet(worksheet)
+
+        del wb["Sheet"]
+        wb.save("style.xlsx")
+
+    def merge_cells(self):
+        # wb = Workbook()
+        wb = openpyxl.load_workbook("style.xlsx")
+        # ws = wb[months[0]]
+        # ws = wb.get_sheet_by_name["January"]
+        ws = wb["January"]
+        #  ustaw zmiena sheet np na luty.
+        ws.merge_cells("A1:E1")
+        ws.merge_cells("A3:C3")
+        ws.merge_cells("A4:B4")
+        ws.merge_cells("A1:B1")
+        ws.merge_cells("A6:C6")
+        ws.merge_cells("A7:C7")
+        ws.merge_cells("A8:C8")
+        ws.merge_cells("A9:C9")
+        ws.merge_cells("A10:C10")
+        ws.merge_cells("A11:C11")
+
+        wb.save("style.xlsx")
+
+        # def copy(self):
+        # wb = openpyxl.load_workbook("style.xlsx")
+        # ws = wb["January"]
+
+        # def sheetnames(self):
+        # wb = openpyxl.load_workbook("style.xlsx")
+        # for sheet in wb.worksheets:
+        # print(wb.sheetnames)
+
+    def table(self):
+        wb = openpyxl.load_workbook("style.xlsx")
+        ws = wb.active
+        tab = Table(displayName="Table1", ref="A1:E5")
+
+        # Add a default style with striped rows and banded columns
+        style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
+                               showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+        tab.tableStyleInfo = style
+
+        wb.save("style.xlsx")
+
+
+
+
+styl = Style()
+styl.create_months()
+styl.merge_cells()
+styl.table()
+
+
+
+# styl.sheetnames()
+
+# tod zrob wzor arkusza w openpyxl
+# tod zrob kopiowanie arkusza na kolejne miesiace
 # class Style:
 #     def __init__(self, Workbook, ws):
 #         self.Workbook = Workbook()
@@ -65,141 +135,3 @@ wb.save("style.xlsx")
 # ws1.merge_cells("A1:E1")
 # wb.save("style.xlsx")
 #######################
-class Style:
-    def create_months(self, wb=Workbook()):
-        # wb = Workbook()
-        # ws = wb.active
-        months = [calendar.month_name[i] for i in range(1, 13)]
-
-        for worksheet in months:
-            wb.create_sheet(worksheet)
-
-        del wb["Sheet"]
-        wb.save("style.xlsx")
-
-    def merge_cells(self):
-        # wb = Workbook()
-        wb = openpyxl.load_workbook("style.xlsx")
-        # ws = wb[months[0]]
-        # ws = wb.get_sheet_by_name["January"]
-        ws = wb["January"]
-        #  ustaw zmiena sheet np na luty.
-        ws.merge_cells("A1:E1")
-        ws.merge_cells("A3:C3")
-        ws.merge_cells("A4:B4")
-        ws.merge_cells("A1:B1")
-        ws.merge_cells("A6:C6")
-        ws.merge_cells("A7:C7")
-        ws.merge_cells("A8:C8")
-        ws.merge_cells("A9:C9")
-        ws.merge_cells("A10:C10")
-        ws.merge_cells("A11:C11")
-
-        wb.save("style.xlsx")
-
-        # def copy(self):
-        # wb = openpyxl.load_workbook("style.xlsx")
-        # ws = wb["January"]
-
-        # def sheetnames(self):
-        # wb = openpyxl.load_workbook("style.xlsx")
-        # for sheet in wb.worksheets:
-        # print(wb.sheetnames)
-
-
-class Text:
-    def card_name(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A1'] = 'ROZLICZENIE MIESIĘCZNE ZUŻYCIA PALIWA'
-        wb.save("style.xlsx")
-
-    def brand(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A3'] = 'Pojazd służbowy, marka'
-        wb.save("style.xlsx")
-
-    def registration_number(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A4'] = 'Nr rejestracyjny'
-        wb.save("style.xlsx")
-
-    def month(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A5'] = 'Miesiąc'
-        ws['B5'] = wb.sheetnames[0]
-        wb.save("style.xlsx")
-
-    def year(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['C5'] = 'rok'
-        year = datetime.datetime.today().year
-
-        ws['D5'] = year
-        wb.save("style.xlsx")
-
-    def count_beginning_month(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A6'] = '1. Stan licznika na początku miesiąca'
-        wb.save("style.xlsx")
-
-    def count_ending_month(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A7'] = '2. Stan licznika na końcu miesiąca'
-        wb.save("style.xlsx")
-
-    def km_in_month(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A8'] = '3. Przejechano km/mth w miesiącu'
-        wb.save("style.xlsx")
-
-    def fuel__reamaining_last_month(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A9'] = '4.	Stan paliwa pozostałego z ubiegłego miesiąca'
-        wb.save("style.xlsx")
-
-    def fuel_purchased_in_month(self):
-        wb = openpyxl.load_workbook("style.xlsx")
-        ws = wb.active
-
-        ws['A10'] = '5.Ilość paliwa zakupionego w miesiącu '
-        wb.save("style.xlsx")
-
-
-styl = Style()
-styl.create_months()
-styl.merge_cells()
-
-text = Text()
-text.card_name()
-text.brand()
-text.registration_number()
-text.month()
-text.year()
-text.count_beginning_month()
-text.count_ending_month()
-text.fuel__reamaining_last_month()
-text.fuel_purchased_in_month()
-text.km_in_month()
-
-# styl.sheetnames()
-
-# tod zrob wzor arkusza w openpyxl
-# tod zrob kopiowanie arkusza na kolejne miesiace
