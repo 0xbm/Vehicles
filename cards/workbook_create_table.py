4ab2ff2cbc67bd39c782446018bbdefae6bfe5db
0xbm <0xmb@protonmail.com>
2023-04-15 16:50:35 +0200

    add new func /signatures_table/

--- cards/workbook_create_table.py
+++ cards/workbook_create_table.py
@@ -7,7 +7,7 @@ ws = wb.active
 
 
 class CreateTable:
-    def table(self):
+    def tank_table(self):
         border_thin = borders.Side(style=None, color='FF000000', border_style='thin')
         border0 = borders.Side(style=None, color=None, border_style=None)
         thin = Border(left=border_thin, right=border_thin, bottom=border_thin, top=border_thin)
@@ -15,9 +15,19 @@ class CreateTable:
         for row in ws.iter_rows(min_row=12, min_col=2, max_row=22, max_col=5):
             for cell in row:
                 cell.border = thin
+    def signatures_table(self):
+        border_thin = borders.Side(style=None, color='FF000000', border_style='thin')
+        border0 = borders.Side(style=None, color=None, border_style=None)
+        thin = Border(left=border_thin, right=border_thin, bottom=border_thin, top=border_thin)
 
-        wb.save('card.xlsx')
+        for row in ws.iter_rows(min_row=30, min_col=2, max_row=32, max_col=5):
+            for cell in row:
+                cell.border = thin
+
+    #wb.save('card.xlsx')
 
 
 create_table = CreateTable()
-create_table.table()
+create_table.tank_table()
+create_table.signatures_table()
+wb.save('card.xlsx')
