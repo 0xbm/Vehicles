a1c8cd3c3a43c3241dcd1540da5fb16698170541
0xbm <0xmb@protonmail.com>
2023-04-29 19:41:59 +0200

    add html view on web browser

--- automobiles/views.py
+++ automobiles/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("Hello, you are at the automobile index.")
+
 def index(request):
-    return HttpResponse("Hello, you are at the automobile index.")
+    return render(request, "index.html")
+
+# todo dodaj css do widoku
+# todo przezuc style z index.html do css
+# todo dodaj btn obok edit (+) albo u gory
