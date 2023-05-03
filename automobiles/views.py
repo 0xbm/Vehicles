8fee209c65a9d7840234b60ab4c953e36d6b3662
0xbm <0xmb@protonmail.com>
2023-05-03 13:45:07 +0200

    remove #todos

--- automobiles/views.py
+++ automobiles/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 # def index(request):
 #     return HttpResponse("Hello, you are at the automobile index.")
 
+
 def index(request):
     return render(request, "index.html")
-
-# todo dodaj btn obok edit (+) albo u gory
-# todo dodaj mozliwosc rejestracji (register , login)
