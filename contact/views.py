4faf863e33c1cbae27b786c8c2a60e649db935b4
0xbm <0xmb@protonmail.com>
2023-05-10 19:50:29 +0200

    redirect to main site after send mail

--- contact/views.py
+++ contact/views.py
@@ -4,7 +4,14 @@ from .forms import ContactForm
 
 # Create your views here.
 def index(request):
-    form = ContactForm(request.POST)
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            print("OK")
+            return redirect("automobiles:index")
+    else:
+        form = ContactForm()
 
     context = {"form": form}
     return render(request, "contact/index.html", context)
