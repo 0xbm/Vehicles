fbdd1a3b7268fdf3cfb30cbcd0dd3e3de545ffc9
0xbm <0xmb@protonmail.com>
2023-05-14 16:58:45 +0200

    necessary variables to receive e-mails

--- contact/views.py
+++ contact/views.py
@@ -1,14 +1,26 @@
+from django.template.loader import render_to_string
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from .forms import ContactForm
 
 
-# Create your views here.
 def index(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
 
         if form.is_valid():
-            print("OK")
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            content = form.cleaned_data['content']
+
+            html = render_to_string('contact/emails/contactform.html', {
+                'name': name,
+                'email': email,
+                'content': content
+            })
+
+            send_mail("The contact from subject", "This is the message", 'noreply@gmail.com', ['automobile@gmail.com'],
+                      html_message=html)
             return redirect("automobiles:index")
     else:
         form = ContactForm()
