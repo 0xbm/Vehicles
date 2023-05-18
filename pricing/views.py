f1642857077779cc60aa1c0111aba5a6772c8444
0xbm <0xmb@protonmail.com>
2023-05-18 19:57:22 +0200

    add next site with pay method and change func name

--- pricing/views.py
+++ pricing/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def pricing(request):
     return render(request, 'pricing/index.html')
+
+
+def pay(request):
+    return render(request, 'pay/index.html')
