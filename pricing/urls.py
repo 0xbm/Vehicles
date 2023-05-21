006963766316a134fb8b7878e7c009db72a80d10
0xbm <0xmb@protonmail.com>
2023-05-21 16:00:17 +0200

    add pay options (3/6/9/12)

--- pricing/urls.py
+++ pricing/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'pricing'
 urlpatterns = [
     path("", views.pricing, name="pricing"),
-    path("pay", views.pay, name="pay"),
+    path("pay_3", views.pay_3, name="pay_3"),
+    path("pay_6", views.pay_6, name="pay_6"),
+    path("pay_9", views.pay_9, name="pay_9"),
+    path("pay_12", views.pay_12, name="pay_12"),
 ]
--- pricing/views.py
+++ pricing/views.py
@@ -7,5 +7,17 @@ def pricing(request):
     return render(request, 'pricing/index.html')
 
 
-def pay(request):
-    return render(request, 'pay/index.html')
+def pay_3(request):
+    return render(request, 'pay/pay_3.html')
+
+
+def pay_6(request):
+    return render(request, 'pay/pay_6.html')
+
+
+def pay_9(request):
+    return render(request, 'pay/pay_9.html')
+
+
+def pay_12(request):
+    return render(request, 'pay/pay_12.html')
