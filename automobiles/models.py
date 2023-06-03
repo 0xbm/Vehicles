128bf2bad356a8bab219258fb8c9e02b11e3db72
0xbm <0xmb@protonmail.com>
2023-06-03 10:18:24 +0200

    add technical inspection field

--- automobiles/models.py
+++ automobiles/models.py
@@ -4,9 +4,19 @@ from django.db import models
 # Create your models here.
 
 class Vehicle(models.Model):
+    # id =
+    name = models.CharField('Name', max_length=20, default=None, blank=True)
     brand = models.CharField('Brand', max_length=10)
     model = models.CharField('Model', max_length=10)
     reg_number = models.CharField('Registration Number', max_length=10)
+    tech_insp = models.DateTimeField("Technical Inspection")
 
-    # def __str__(self):
-    #     return self.brand
+    def full(self):
+        return f"{self.brand} {self.model}".title()
+
+    def __str__(self):
+        # self.name = self.name.title()
+        return self.full()
+
+# class Brand(models.Model):
+#     brand = models.CharField('Brand', max_length=10)
