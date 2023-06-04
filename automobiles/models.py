e3131e2c3cf6f347c85e6b7b6a0d4164aad2bf3b
0xbm <0xmb@protonmail.com>
2023-06-04 19:17:59 +0200

    create 3 tables in sqlite

--- automobiles/models.py
+++ automobiles/models.py
@@ -3,20 +3,43 @@ from django.db import models
 
 # Create your models here.
 
-class Vehicle(models.Model):
-    # id =
-    name = models.CharField('Name', max_length=20, default=None, blank=True)
-    brand = models.CharField('Brand', max_length=10)
-    model = models.CharField('Model', max_length=10)
-    reg_number = models.CharField('Registration Number', max_length=10)
-    tech_insp = models.DateTimeField("Technical Inspection")
 
-    def full(self):
+class Brand(models.Model):
+    brand = models.CharField('Brand', max_length=10)
+
+    # def brand_capitalize(self):
+    #     return f"{self.brand_capitalize}".title()
+
+    def __str__(self):
+        # self.name = self.name.title()
+        return self.brand
+
+
+class Model(models.Model):
+    model = models.CharField('Model', max_length=10)
+
+    # def model_capitalize(self):
+    #     return f"{self.model_capitalize}".title()
+
+    def __str__(self):
+        # self.name = self.name.title()
+        return self.model
+
+
+class Vehicle(models.Model):
+    name = models.CharField('Name', max_length=20)
+    brand = models.ManyToManyField(Brand, blank=True)
+    model = models.ManyToManyField(Model, blank=True)
+    # brand = models.CharField(max_length=10)
+    # model = models.CharField(max_length=10)
+    reg_number = models.CharField('Registration Number', max_length=10)
+    tech_insp = models.DateField("Technical Inspection")
+    # driver = models.CharField('Driver', max_length=20)
+    # description = models.TextField(blank=True)
+
+    def full_name(self):
         return f"{self.brand} {self.model}".title()
 
     def __str__(self):
         # self.name = self.name.title()
-        return self.full()
-
-# class Brand(models.Model):
-#     brand = models.CharField('Brand', max_length=10)
+        return self.full_name()
