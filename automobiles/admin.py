8c964bfc97a332ffa9b9361a9009ad77141137dc
0xbm <0xmb@protonmail.com>
2023-06-09 17:36:27 +0200

    search and filter admin account

--- automobiles/admin.py
+++ automobiles/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Brand, Model, Vehicle
+from .models import Brand, Model, Vehicle, Driver
 
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(Model)
-admin.site.register(Vehicle)
+admin.site.register(Driver)
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
+    list_filter = ('brand', 'model', 'driver')
