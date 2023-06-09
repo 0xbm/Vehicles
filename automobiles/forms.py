7ea3156505c9193ef26485fcac7cdc9b253fe57f
0xbm <0xmb@protonmail.com>
2023-06-09 17:37:11 +0200

    add bootstrap widgets and placeholder

--- automobiles/forms.py
+++ automobiles/forms.py
@@ -4,9 +4,28 @@ from .models import Vehicle
 
 
 class VehicleForm(ModelForm):
-    name = forms.TextInput()
-
     class Meta:
         model = Vehicle
-        fields = ['name', 'brand', 'model', 'reg_number', 'tech_insp', 'vehicle_image']
+        fields = ['name', 'brand', 'model', 'reg_num', 'tech_insp', 'driver', 'vehicle_image', 'description']
 
+        labels = {
+            'name': '',
+            'brand': '',
+            'model': '',
+            'reg_num': '',
+            'tech_insp': '',
+            'driver': '',
+            'vehicle_image': '',
+            'description': '',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Vehicle Name'}),
+            'brand': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Vehicle Brand'}),
+            'model': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Vehicle Model'}),
+            'reg_num': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Registry Number'}),
+            'tech_insp': forms.DateInput(
+                attrs={'class': 'form-control', 'placeholder': 'Technical Inspection Date (YYYY:MM:DD)'}),
+            'driver': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Driver'}),
+            # 'vehicle_image': forms.TextInput(attrs={'class': 'form-control','placeholder':'Vehicle Name'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+        }
