77bc8dc0ddfe09ae1371b0d0d3ddadbf751e0aa6
0xbm <0xmb@protonmail.com>
2023-06-12 19:08:29 +0200

    add to name .title()

--- automobiles/models.py
+++ automobiles/models.py
@@ -29,11 +29,11 @@ class Vehicle(models.Model):
     brand = models.ForeignKey(Brand, blank=True, null=True, on_delete=models.CASCADE)  # null=True
     model = models.ForeignKey(Model, blank=True, null=True, on_delete=models.CASCADE)  # null=True
     reg_num = models.CharField('Registration Number', max_length=10, blank=True)
-    tech_insp = models.DateField("Technical Inspection", blank=True)
+    tech_inspection = models.DateField("Technical Inspection", blank=True, null=True)
     vin_num = models.CharField('VIN Number', max_length=17, blank=True)
     driver = models.ManyToManyField(Driver, blank=True)
     description = models.TextField(blank=True)
-    vehicle_image = models.ImageField(blank=True, null=True, upload_to="images/")
+    vehicle_image = models.ImageField(blank=True, upload_to="images/")
 
     def __str__(self):
         return self.name.title()
