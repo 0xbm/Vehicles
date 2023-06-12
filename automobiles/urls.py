45abf7b935d8e11700d122a54ce99bcf141be2c1
0xbm <0xmb@protonmail.com>
2023-06-12 19:06:35 +0200

    add options : UPDATE vehicle

--- automobiles/urls.py
+++ automobiles/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("add_vehicle/", views.add_vehicle, name="add_vehicle"),
     # path("contact/", views.contact, name="contact"), # to jest contact z html form (ladny)
+    path("all_vehicles/", views.all_vehicles, name="all_vehicles"),
     path("list_vehicles/", views.list_vehicles, name="list_vehicles"),
+    path("show_vehicle/<vehicle_id>", views.show_vehicle, name="show_vehicle"),
+    path("search_vehicles/", views.search_vehicles, name="search_vehicles"),
+    path("update_vehicle/<vehicle_id>", views.update_vehicle, name="update_vehicle"),
+
 ]
--- automobiles/views.py
+++ automobiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import VehicleForm
 from datetime import datetime
-from .models import Brand, Model, Vehicle
+from .models import Brand, Model, Vehicle, Driver
 from django.http import HttpResponseRedirect
 
 
@@ -13,7 +13,7 @@ from django.http import HttpResponseRedirect
 def home(request):
     now = datetime.now()
     current_year = now.year
-    time = now.strftime('%I:%M:%S %p')
+    time = now.strftime('%H:%M:%S')
     context = {"current_year": current_year, 'time': time}
     return render(request, "home.html", context)
 
@@ -25,7 +25,6 @@ def add_vehicle(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('?submitted=True')
-        # return redirect('automobiles:add_vehicle')
     else:
         form = VehicleForm()
         if 'submitted' in request.GET:
@@ -38,10 +37,57 @@ def add_vehicle(request):
     return render(request, "add_vehicle.html", context)
 
 
-def list_vehicles(request):
-    vehicle_list = Vehicle.objects.all()
+def all_vehicles(request):
+    vehicles_all = Vehicle.objects.all()
     now = datetime.now()
     current_year = now.year
-    time = now.strftime('%I:%M:%S %p')
-    context = {'vehicle_list': vehicle_list, "current_year": current_year, 'time': time}
+    time = now.strftime('%H:%M:%S')
+    context = {'vehicles_all': vehicles_all, "current_year": current_year, 'time': time}
+    return render(request, 'all_vehicles.html', context)
+
+
+def list_vehicles(request):
+    vehicles_list = Vehicle.objects.all()
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime('%H:%M:%S')
+    context = {'vehicles_list': vehicles_list, "current_year": current_year, 'time': time}
     return render(request, 'list_vehicles.html', context)
+
+
+def show_vehicle(request, vehicle_id):
+    vehicle_show = Vehicle.objects.get(pk=vehicle_id)
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime('%H:%M:%S')
+    context = {'vehicle_show': vehicle_show, "current_year": current_year, 'time': time}
+    return render(request, 'show_vehicle.html', context)
+
+
+def search_vehicles(request):
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime('%H:%M:%S')
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        vehicles = Vehicle.objects.filter(name__contains=searched)
+        return render(request, 'search_vehicles.html',
+                      {'searched': searched, 'vehicles': vehicles, "current_year": current_year, 'time': time})
+    else:
+        vehicles_search = Vehicle.objects.all()
+        context = {'vehicles_search': vehicles_search, "current_year": current_year, 'time': time}
+        return render(request, 'search_vehicles.html', context)
+
+
+def update_vehicle(request, vehicle_id):
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime('%H:%M:%S')
+    vehicle_update = Vehicle.objects.get(pk=vehicle_id)
+    form = VehicleForm(request.POST or None, instance=vehicle_update)
+    if form.is_valid():
+        form.save()
+        redirect('update_vehicle.html')
+
+    context = {"form": form, 'vehicle_update': vehicle_update, "current_year": current_year, 'time': time}
+    return render(request, 'update_vehicle.html', context)
