d1ea3bce8a4154f5a528df3ffd34080d847b4a76
0xbm <0xmb@protonmail.com>
2023-06-12 19:07:12 +0200

    change forms

--- automobiles/forms.py
+++ automobiles/forms.py
@@ -6,26 +6,28 @@ from .models import Vehicle
 class VehicleForm(ModelForm):
     class Meta:
         model = Vehicle
-        fields = ['name', 'brand', 'model', 'reg_num', 'tech_insp', 'driver', 'vehicle_image', 'description']
-
+        fields = ['name', 'brand', 'model', 'reg_num', 'vin_num', 'tech_inspection', 'driver', 'vehicle_image',
+                  'description']
         labels = {
             'name': '',
-            'brand': '',
-            'model': '',
+            'brand': 'Brand',
+            'model': 'Model',
             'reg_num': '',
-            'tech_insp': '',
+            'vin_num': '',
+            'tech_inspection': '',
             'driver': '',
             'vehicle_image': '',
             'description': '',
         }
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Vehicle Name'}),
-            'brand': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Vehicle Brand'}),
-            'model': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Vehicle Model'}),
+            'brand': forms.Select(attrs={'class': 'form-select', 'placeholder': 'Vehicle Brand'}),
+            'model': forms.Select(attrs={'class': 'form-select', 'placeholder': 'Vehicle Model'}),
             'reg_num': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Registry Number'}),
-            'tech_insp': forms.DateInput(
+            'vin_num': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'VIN'}),
+            'tech_inspection': forms.DateInput(
                 attrs={'class': 'form-control', 'placeholder': 'Technical Inspection Date (YYYY:MM:DD)'}),
-            'driver': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Driver'}),
-            # 'vehicle_image': forms.TextInput(attrs={'class': 'form-control','placeholder':'Vehicle Name'}),
-            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+            'driver': forms.SelectMultiple(attrs={'class': 'form-control', 'placeholder': 'Driver'}),
+            'vehicle_image': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Vehicle Image'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
         }
